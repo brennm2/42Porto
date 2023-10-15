5d18955d6f2676c71137299bbd88db265cda61f4
Brendon Vianna <127636164+brennm2@users.noreply.github.com>
2023-10-15 19:40:36 +0100

    feito em casa

---
+++ ft_strjoin.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bde-souz <bde-souz@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/13 16:14:38 by bde-souz          #+#    #+#             */
+/*   Updated: 2023/10/13 16:15:33 by bde-souz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	int		strlen;
+	int		i;
+	int		j;
+	char	*str;
+
+	i = 0;
+	j = 0;
+	strlen = ft_strlen(s1) + ft_strlen(s2);
+	str = (char *)malloc(sizeof(char) * (strlen + 1));
+	if (!str)
+		return (NULL);
+	while (s1 && s2)
+	{
+		while (s1[i])
+			str[j++] = s1[i++];
+		i = 0;
+		while (s2[i])
+			str[j++] = s2[i++];
+		str[j] = '\0';
+		return (str);
+	}
+	return (NULL);
+}
+
+// #include <stdio.h>
+
+// int	main(void)
+// {
+// 	printf("%s\n", ft_strjoin("123", "456"));
+// 	return (0);
+// }
